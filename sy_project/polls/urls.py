0bd56a8657847d050333477524df2224b79483fd
Dinosy <asn6878@gmail.com>
2023-03-09 01:43:09 +0900

    index, detail html updated, also http404 response exception included.

--- sy_project/polls/urls.py
+++ sy_project/polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# 아래와같이 urls.py 에 namespace를 할당해줌으로써 탬플릿태그 {% url %} 을 사용할때 어떤 앱의 뷰에서 url을 생성할지 알 수 있다.
+app_name = 'polls' 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
--- sy_project/polls/views.py
+++ sy_project/polls/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+# from django.template import loader
+
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #template = loader.get_template('polls/index.html')
+    #return HttpResponse(template.render(context, request)) #다음과 같이 HttpResponse로 돌려줄 수도있지만 매번 하기 번거로워 django의 shortcut을 이용한다.
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("해당 Index의 Question은 존재하지 않습니다!")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of questions %s."
